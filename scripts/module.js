28a25220b66a75767d76add0a873e0214d31e085
Nicolas Grobelny <miki4920@gmail.com>
2021-05-26 13:26:30 +0100

    Added an option for directory picking
    User can now pick a destination directory from the config menu

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,12 @@
+import DirectoryPicker from "./lib/directory_picker.js";
+
 Hooks.once('init', async function() {
-
-});
-
-Hooks.once('ready', async function() {
-
+    game.settings.register("foundryvtt-battlemaps-integration", "mapDirectory", {
+        name: "Download Directory",
+        hint: "Set a Directory for downloaded Battlemaps",
+        scope: "world",
+        config: true,
+        type: DirectoryPicker.Directory,
+        default: "",
+    });
 });
